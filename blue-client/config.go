ef46c244fba5b9f1eb0fe71ebde694ede8208870
苟杰 <507762441@qq.com>
2024-03-10 00:30:29 +0800

    add: cli config file

--- blue-client/config.go
+++ blue-client/config.go
@@ -1,11 +1,21 @@
 package main
 
-var BC BlueConf
+import (
+	"bufio"
+	"io"
+	"os"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+var BC = new(BlueConf)
 
 type BlueConf struct {
-	Ip      string `json:"ip,omitempty"`
-	Port    int    `json:"port,omitempty"`
-	TimeOut uint16 `json:"time_out"`
+	Ip       string `json:"ip,omitempty"`
+	Port     int    `json:"port,omitempty"`
+	TimeOut  int    `json:"time_out"`
+	TryTimes int    `json:"try_times"`
 
 	LogOut string `json:"log_out,omitempty"`
 }
@@ -15,4 +25,81 @@ var defaultConf = BlueConf{
 	Port:    8080,
 	TimeOut: 60,
 	LogOut:  "./log",
+}
+
+func init() {
+	conf, err := os.Open("./blue-cli.conf")
+	if err != nil {
+		panic(err)
+	}
+	defer conf.Close()
+
+	red := bufio.NewReader(conf)
+
+	for {
+		readString, err := red.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			panic(err)
+		}
+
+		if readString == "" {
+			continue
+		}
+
+		parse(readString, BC)
+
+	}
+}
+
+func parse(str string, conf *BlueConf) {
+	str = strings.TrimSpace(str)
+
+	if strings.HasPrefix(str, "#") || strings.HasPrefix(str, "//") {
+		return
+	}
+
+	split := strings.Split(str, " ")
+	confs := make([]string, 0)
+
+	for i := range split {
+		if split[i] == "" {
+			continue
+		}
+
+		confs = append(confs, split[i])
+		if len(confs) == 2 {
+			break
+		}
+
+	}
+
+	// 通过反射将 confs 赋值给conf
+	elem := reflect.ValueOf(conf).Elem()
+
+	if len(confs) == 2 {
+		field := elem.FieldByName(confs[0])
+
+		if field.IsValid() && field.CanSet() {
+			switch field.Kind() {
+			case reflect.String:
+				field.SetString(confs[1])
+			case reflect.Int:
+				parseInt, err := strconv.Atoi(confs[1])
+				if err == nil {
+					field.SetInt(int64(parseInt))
+				}
+			case reflect.Uint16:
+				parseUint, err := strconv.ParseUint(confs[1], 10, 16)
+				if err == nil {
+					field.SetUint(parseUint)
+				}
+			default:
+				panic("unknown config")
+			}
+		}
+	}
+
 }
--- blue-client/main.go
+++ blue-client/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"blue/bsp"
+	"bufio"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"strings"
 )
 
 var (
-	remoteAddr net.Addr
+	conn net.Conn
 )
 
 func TidyInput(input string) []string {
@@ -25,72 +28,76 @@ func TidyInput(input string) []string {
 	return newSplit
 }
 
-func Connect() net.Conn {
-	d, err := net.Dial("tcp", fmt.Sprintf("%s:%d", BC.Ip, BC.Port))
-	if err != nil {
-		ErrPrint(ErrConnect(err.Error()))
-		os.Exit(1)
-	}
-
-	return d
+func init() {
+	Connect()
 }
 
-//// num get a
-//func main() {
-//	// 从标准输入创建一个新的 bufio.Reader
-//	reader := bufio.NewReader(os.Stdin)
-//	d := Connect()
-//
-//	for {
-//		remoteAddr = d.RemoteAddr()
-//		fmt.Printf("%s> ", remoteAddr.String())
-//		// 读取直到遇到换行符
-//		input, err := reader.ReadString('\n')
-//		if err != nil {
-//			ErrPrint(ErrRead(err.Error()))
-//			continue
-//		}
-//
-//		split := TidyInput(input)
-//
-//		if len(split) == 0 {
-//			continue
-//		}
-//
-//		exec, err := Exec(split)
-//		if err != nil {
-//			ErrPrint(err)
-//			continue
-//		}
-//
-//		//for _, b := range exec {
-//		//	fmt.Printf("[%b]\n", b)
-//		//}
-//		//continue
-//
-//		_, err = d.Write(exec)
-//		if err != nil {
-//			d = Connect()
-//			_, err = d.Write(exec)
-//			if err != nil {
-//				ErrPrint(ErrConnect(err.Error()))
-//			}
-//			continue
-//		}
-//
-//		buf := make([]byte, 1024)
-//		n, err := d.Read(buf)
-//		if err != nil && err != io.EOF {
-//			ErrPrint(ErrConnect(err.Error()))
-//			continue
-//		}
-//		bytes := buf[:n]
-//
-//		resp, err := NewReplyMessage(bytes)
-//		if err != nil {
-//			ErrPrint(err)
-//			continue
-//		}
-//		SuccessPrint(GreenMessage(resp))
-//	}
-//}
+func Connect() {
+	var err error
+	for i := 0; i < BC.TryTimes; i++ {
+		conn, err = net.Dial("tcp", fmt.Sprintf("%s:%d", BC.Ip, BC.Port))
+		if err != nil {
+			ErrPrint(ErrConnect(err.Error()))
+			os.Exit(1)
+		}
+	}
+}
+
+// num get a
+func main() {
+	// 从标准输入创建一个新的 bufio.Reader
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		remoteAddr := conn.RemoteAddr()
+		fmt.Printf("%s> ", remoteAddr.String())
+		// 读取直到遇到换行符
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			ErrPrint(ErrRead(err.Error()))
+			continue
+		}
+
+		split := TidyInput(input)
+
+		if len(split) == 0 {
+			continue
+		}
+
+		exec, err := Exec(split)
+		if err != nil {
+			ErrPrint(err)
+			continue
+		}
+
+		//for _, b := range exec {
+		//	fmt.Printf("[%b]\n", b)
+		//}
+		//continue
+	Resend:
+		_, err = conn.Write(exec)
+		if err != nil {
+			Connect()
+			goto Resend
+		}
+
+		buf := make([]byte, 1024)
+		n, err := conn.Read(buf)
+		if err != nil && err != io.EOF {
+			ErrPrint(ErrConnect(err.Error()))
+			continue
+		}
+		bytes := buf[:n]
+
+		resp, err := bsp.NewReplyMessage(bytes)
+		if err != nil {
+			ErrPrint(err)
+			continue
+		}
+		SuccessPrint(GreenMessage(resp))
+	}
+}
+
+func Exec(s []string) ([]byte, error) {
+	return nil, nil
+}
